70810573960b4d113ef61704a3661a45579d0112
Veshal kumar singh <veshalkumar25@gmail.com>
2023-03-06 15:22:52 +0530

    Add files via upload

---
+++ Data-Hive/Data-Hive App/src/com/datahive/app/DelSurvey.java
@@ -0,0 +1,47 @@
+package com.datahive.app;
+
+import java.sql.*;
+
+
+public class DelSurvey {
+	String username;
+	
+	 DelSurvey(String username) {
+		 this.username = username;
+		 
+    	 Connection conn = null;
+	        String url = "jdbc:mysql://localhost:3305/datahive";
+	        String userId = "root";
+	        String password = "123456";
+	        
+	        try{
+                //Register JDBC Driver
+//                Class.forName("com.mysql.jdbc.Driver");
+
+//Open a connection
+                System.out.println("Connecting to database plz wait....!!");
+                conn = DriverManager.getConnection(url, userId, password);
+                
+                String d = "DELETE FROM details WHERE USERNAME = ?";
+                PreparedStatement pstmt = conn.prepareStatement(d);
+
+//set the value to the query
+                pstmt.setString(1, username);
+                pstmt.executeUpdate();
+                System.out.println("Data DELETED successfully..!");
+                conn.close();
+                
+
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+	 
+		
+	}
+
+	public static void main(String[] args) {
+		DelSurvey d = new DelSurvey("");
+
+	}
+
+}
